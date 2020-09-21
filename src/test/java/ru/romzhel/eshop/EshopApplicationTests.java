9f376d83dde24ecb57b2a7741c7bd90fad1b2b48
Roman Zheludkov <romzhel@mail.ru>
2020-09-21 21:05:16 +0300

    initial commit

---
+++ src/test/java/ru/romzhel/eshop/EshopApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.romzhel.eshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
